c1d6e5a796cdc8476120ccfbae803183f2e5e757
danil_proskurin <danielpashtetov@gmail.com>
2024-12-13 23:10:24 +0700

    fix urls.py

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -23,7 +23,7 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     # drf jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
